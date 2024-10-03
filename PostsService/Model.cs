b63be92e7a8b808b457aba5eb4e9b98094110e9f
AsbjrnJacobsen <megetunikemail@gmail.com>
2024-10-03 10:25:51 +0200

    Model.cs updated - Post's now get DateTime.Now on instantiation.

--- PostsService/Model.cs
+++ PostsService/Model.cs
@@ -7,8 +7,9 @@ public class Post
     [Key]
     [Required]
     public int postID { get; set; }
+    
     [Required]
-    public DateTime timestamp { get; set; }
+    public DateTime timestamp { get; private set;} = DateTime.Now;
     public int? repliedToPostID { get; set; }
     [Required]
     public string username { get; set; }
