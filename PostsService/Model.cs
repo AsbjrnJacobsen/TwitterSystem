602bb1e2672f91cfc8c244d26bf70746e7bbd125
AsbjrnJacobsen <megetunikemail@gmail.com>
2024-10-03 10:33:00 +0200

    repliedToPostID updated

--- PostsService/Model.cs
+++ PostsService/Model.cs
@@ -7,10 +7,9 @@ public class Post
     [Key]
     [Required]
     public int postID { get; set; }
-    
     [Required]
     public DateTime timestamp { get; private set;} = DateTime.Now;
-    public int? repliedToPostID { get; set; }
+    public int? repliedToPostID { get; set; } = -1; //if value is -1 then there is no parent tweet
     [Required]
     public string username { get; set; }
     [Required]
